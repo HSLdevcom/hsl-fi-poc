75a47ca62a5126099ef65436ba8f31beb7475554
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-06-26 14:02:42 +0300

    Nav bar from styled components to render in this poc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { PageTitleBar } from 'sharedcomponents';
+import { PageTitleBar, Nav, Menu, MenuItem, NavItem } from 'sharedcomponents';
+import { HSLLogo, JourneyPlanner, Tickets, CustomerService, Latest, More, TravelCard, SignIn } from 'sharedcomponents/lib/Icons';
 import hslTheme from './themes.hsl.js';
-import logo from './logo.svg';
 import './App.css';
+
+const languages = [
+  { id: 'fi', name: 'FI' },
+  { id: 'sv', name: 'SV' },
+  { id: 'en', name: 'EN' }
+];
+
+const icons = [
+  [<JourneyPlanner height="2.5rem" width="2.5rem" />, 'Reittiopas'],
+  [<Tickets height="2.5rem" width="2.5rem" />, 'Liput ja hinnat'],
+  [<CustomerService height="2.5rem" width="2.5rem" />, 'Asiakaspalvelu'],
+  [<Latest height="2.5rem" width="2.5rem" />, 'Uutta'],
+  [<More height="2.5rem" width="2.5rem" />, 'Lisää']
+];
+const logo = <HSLLogo height="3.75rem" />;
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={hslTheme}>
+
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
+          <Nav
+            logo={logo}
+            menu={
+              <Menu
+                selectedLanguage={'fi'}
+                languages={languages}
+                changeLanguage={(lang) => console.log('lang chage clicked', lang)}
+              >
+                <MenuItem
+                  link={<span />}
+                  icon={<TravelCard height="3.5rem" />}
+                  text="Matkakortti"
+                  textPosition="Right"
+                  key="travelcard"
+                />
+                <MenuItem
+                  link={<span />}
+                  icon={<SignIn height="3.5rem" />}
+                  text="Kirjaudu"
+                  textPosition="Right"
+                  key="signin"
+                />
+              </Menu>
+            }
+          >
+            {icons.map(icon =>
+              (<NavItem
+                key={icon[1]}
+                link={<span />}
+                icon={icon[0]}
+                text={icon[1]}
+                textPosition={'Bottom'}
+              />)
+            )}
+          </Nav>
           <PageTitleBar>
             <h1>Welcome to React</h1>
           </PageTitleBar>
--- src/themes.hsl.js
+++ src/themes.hsl.js
@@ -1,5 +1,52 @@
+import Media from './media-templates';
+
+const primaryBlue = '#007ac9';
+
+const colors = {
+  defaultTextColor: '#999',
+  hslBlue: '#007ac9',
+  errorTitle: '#DC0451'
+};
+
+const fontsizes = {
+  small: '0.9em',
+  medium: '1em',
+  large: '1.1em',
+  xlarge: '1.6em',
+  xxlarge: '2em',
+  xxxlarge: '3em'
+};
+
+const breakpoints = {
+  mobile: '425px',
+  tablet: '768px',
+  laptop: '1024px'
+};
+
 const hslTheme = {
-  primary: '#007ac9'
+  Media,
+  color: 'red',
+  primary: primaryBlue,
+  primaryText: '#fff',
+  primaryHover: '#0565a3',
+  secondary: '#888',
+  background: primaryBlue,
+  menuBorder: '#0062a1',
+  success: '#64be14',
+  error: '#DB4F4F',
+  info: '#58abc3',
+  md: '900px',
+  navbarHeight: '5rem',
+  logoHeight: '4rem',
+  logoFill: '#fff',
+  radioBtnActive: '#0062a1',
+  fontFamily: '"Gotham Rounded SSm A","Gotham Rounded SSm B"',
+  fontSize: '16px',
+  fontWeight: '500',
+  letterSpacing: '-0.5px',
+  colors,
+  fontsizes,
+  breakpoints
 };
 
 export default hslTheme;
